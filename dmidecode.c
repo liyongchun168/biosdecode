b60d0dc7211cd6f398c4c000b758eb93d1129d70
liyongchun168 <liyongchun168@yeah.net>
2018-11-16 20:19:13 +0800

    0.2

--- dmidecode.c
+++ dmidecode.c
@@ -123,8 +123,19 @@
 #include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
+#include <getopt.h>
+
+
+#include "dmiopt.h"
+#include "config.h"
+
+
 
 #define VERSION "1.8"
+
+/* Options are global */
+struct opt opt;
+
 
 typedef unsigned char u8;
 typedef unsigned short u16;
@@ -1535,191 +1546,408 @@ static __inline__ int checksum(u8 *buf, int len)
 	return (sum==0);
 }
 
-int main(__attribute__ ((unused)) int argc, const char *argv[])
+/*
+ * Handling of option --type
+ */
+
+struct type_keyword
 {
-	u8 buf[48];
-	int fd=open("/dev/mem", O_RDONLY);
-	long fp=0xE0000L;
-	u8 smmajver=0, smminver=0;
-	
-	if(sizeof(u8)!=1 || sizeof(u16)!=2 || sizeof(u32)!=4 || '\0'!=0)
-	{
-		fprintf(stderr,"%s: compiler incompatibility.\n", argv[0]);
-		exit(255);
-	}
-	printf("# dmidecode %s\n", VERSION);
-	if(fd==-1)
-	{
-		perror("/dev/mem");
-		exit(1);
-	}
-	if(lseek(fd, fp, SEEK_SET)==-1)
-	{
-		perror("lseek");
-		exit(1);
-	}
-		
-	while( fp < 0xFFFFF)
-	{
-		ssize_t r=0, r2=0;
-		
-		while(r2!=16 && (r=read(fd, buf+r2, 16-r2))!=0 && r!=-1)
-			r2+=r;
-		if(r==-1)
-		{
-			perror("read");
-			exit(1);
-		}
-		if(r==0)
-		{
-			fputs("read: Unexpected end of file\n", stderr);
-			exit(1);
-		}
-		
-		if(memcmp(buf, "_SM_", 4)==0)
-			printf("SMBIOS %d.%d present.\n", smmajver=buf[6], smminver=buf[7]);
-		else if(memcmp(buf, "_SYSID_", 7)==0)
-		{
-			/* complete buffer, compute checksum */
-			read(fd, buf+16, 16); /* arbitrary, at least 1 */
-			if(*(u16 *)(buf+8)<=32 && checksum(buf, *(u16 *)(buf+8)))
-			{
-				fp+=16;
-				printf("SYSID present.\n");
-				printf("\tRevision: %u\n",
-					buf[16]);
-				printf("%d structure%s.\n",
-					*(u16 *)(buf+14), *(u16 *)(buf+14)>1?"s":"");
-				printf("SYSID table at 0x%08X.\n",
-					*(u32 *)(buf+10));
-			}
-			else
-				lseek(fd, -16, SEEK_CUR);
-		}
-		else if(memcmp(buf, "_DMI_", 5)==0 && checksum(buf, 15))
-		{
-			u16 num=buf[13]<<8|buf[12];
-			u16 len=buf[7]<<8|buf[6];
-			u32 base=buf[11]<<24|buf[10]<<16|buf[9]<<8|buf[8];
+	const char *keyword;
+	const u8 *type;
+};
 
-			/* if version is 0.0, the real version is taken from _SM_ above */
-			printf("DMI %d.%d present.\n",
-				buf[14]?buf[14]>>4:smmajver, buf[14]?buf[14]&0x0F:smminver);
-			printf("%d structures occupying %d bytes.\n",
-				num, len);
-			printf("DMI table at 0x%08X.\n",
-				base);
-			dmi_table(fd, base, len, num);
+static const u8 opt_type_bios[] = { 0, 13, 255 };
+static const u8 opt_type_system[] = { 1, 12, 15, 23, 32, 255 };
+static const u8 opt_type_baseboard[] = { 2, 10, 41, 255 };
+static const u8 opt_type_chassis[] = { 3, 255 };
+static const u8 opt_type_processor[] = { 4, 255 };
+static const u8 opt_type_memory[] = { 5, 6, 16, 17, 255 };
+static const u8 opt_type_cache[] = { 7, 255 };
+static const u8 opt_type_connector[] = { 8, 255 };
+static const u8 opt_type_slot[] = { 9, 255 };
 
-			/* dmi_table moved us far away */
-			lseek(fd, fp+16, SEEK_SET);
-		}
-		else if(memcmp(buf, "$PnP", 4)==0)
+static const struct type_keyword opt_type_keyword[] = {
+	{ "bios", opt_type_bios },
+	{ "system", opt_type_system },
+	{ "baseboard", opt_type_baseboard },
+	{ "chassis", opt_type_chassis },
+	{ "processor", opt_type_processor },
+	{ "memory", opt_type_memory },
+	{ "cache", opt_type_cache },
+	{ "connector", opt_type_connector },
+	{ "slot", opt_type_slot },
+};
+
+static void print_opt_type_list(void)
+{
+	unsigned int i;
+
+	fprintf(stderr, "Valid type keywords are:\n");
+	for (i = 0; i < ARRAY_SIZE(opt_type_keyword); i++)
+	{
+		fprintf(stderr, "  %s\n", opt_type_keyword[i].keyword);
+	}
+}
+
+static u8 *parse_opt_type(u8 *p, const char *arg)
+{
+	unsigned int i;
+
+	/* Allocate memory on first call only */
+	if (p == NULL)
+	{
+		p = (u8 *)calloc(256, sizeof(u8));
+		if (p == NULL)
 		{
-			/* complete buffer, compute checksum */
-			read(fd, buf+16, 32); /* arbitrary, at least 1 */
-			if(buf[5]<=48 && checksum(buf, buf[5]))
-			{
-				fp+=32;
-				printf("PNP %u.%u present.\n",
-					buf[4]>>4, buf[4]&0x0F);
-				printf("\tEvent Notification: %s\n",
-					pnp_event_notification((*(u16 *)(buf+6))&0x03));
-				if(((*(u16 *)(buf+6))&0x03)==1)
-					printf("\tEvent Notification Flag Address: 0x%08X\n",
-						*(u32 *)(buf+9));
-				printf("\tReal Mode Code Address: %04X:%04X\n",
-					*(u16 *)(buf+0x0F), *(u16 *)(buf+0x0D));
-				printf("\tReal Mode Data Address: %04X:0000\n",
-					*(u16 *)(buf+0x1B));
-				printf("\tProtected Mode Code Address: 0x%08X\n",
-					*(u32 *)(buf+0x13)+*(u16 *)(buf+0x11));
-				printf("\tProtected Mode Data Address: 0x%08X\n",
-					*(u32 *)(buf+0x1D));
-				if(*(u32 *)(buf+0x17)!=0)
-					printf("\tOEM Device Identifier: 0x%08X\n",
-						*(u32 *)(buf+0x17));
-			}
-			else
-				lseek(fd, -32, SEEK_CUR);
+			perror("calloc");
+			return NULL;
 		}
-		else if(memcmp(buf, "RSD PTR ", 8)==0)
-		{
-			/* complete buffer, compute checksum */
-			read(fd, buf+16, 4);
-			if(checksum(buf, 20))
-			{
-				lseek(fd, 12, SEEK_CUR);
-				fp+=16;
-				printf("ACPI%s present.\n",
-					acpi_version(buf[15]));
-				printf("\tOEM ID: ");
-				fwrite(buf+9, 6, 1, stdout);
-				printf("\n");
-				printf("RSD table at 0x%08X.\n",
-					*(u32 *)(buf+16));
-			}
-			else
-				lseek(fd, -4, SEEK_CUR);
-		}
-		else if(memcmp(buf, "$SNY", 4)==0)
-		{
-			/* complete buffer, compute checksum */
-			read(fd, buf+16, 16);
-			if(buf[5]<=32 && checksum(buf, buf[5]))
-			{
-				fp+=16;
-				printf("Sony system detected (?).\n");
-			}
-			else
-				lseek(fd, -16, SEEK_CUR);
-		}
-		else if(memcmp(buf, "_32_", 4)==0)
-		{
-			/* complete buffer, reposition file, compute checksum */
-			read(fd, buf+16, 32);
-			lseek(fd, -32, SEEK_CUR);
-			if(buf[9]>=1 && buf[9]<=3 && checksum(buf, buf[9]<<4))
-			{
-				printf("BIOS32 Service Directory present.\n");
-				printf("\tCalling Interface Address: 0x%08X\n",
-					*(u32 *)(buf+4));
-				lseek(fd, (buf[9]-1)<<4, SEEK_CUR);
-				fp+=(buf[9]-1)<<4;
-			}
-		}
-		else if(memcmp(buf, "$PIR", 4)==0)
-		{
-			/* complete buffer, reposition file */
-			read(fd, buf+16, 16);
-			lseek(fd, -16, SEEK_CUR);
-			if(buf[5]!=0 && buf[30]==0)
-			{
-				printf("PCI Interrupt Routing %u.%u present.\n",
-					buf[5], buf[4]);
-				printf("\tTable Size: %u bytes\n",
-					*(u16 *)(buf+6));
-				printf("\tRouter ID: %02x:%02x.%1x\n",
-					buf[8], buf[9]>>3, buf[9]&0x07);
-				printf("\tExclusive IRQs:");
-				pir_exclusive_irqs(*(u16 *)(buf+10));
-				printf("\n");
-				if(*(u32 *)(buf+12)!=0)
-					printf("\tCompatible Router: %04x:%04x\n",
-						*(u16 *)(buf+12), *(u16 *)(buf+14));
-				if(*(u32 *)(buf+16)!=0)
-					printf("\tMiniport Data: 0x%08X\n",
-						*(u32 *)(buf+16));
-			}
-		}
-		fp+=16;
 	}
 
-	if(close(fd)==-1)
+	/* First try as a keyword */
+	for (i = 0; i < ARRAY_SIZE(opt_type_keyword); i++)
 	{
-		perror("/dev/mem");
-		exit(1);
+		if (!strcasecmp(arg, opt_type_keyword[i].keyword))
+		{
+			int j = 0;
+			while (opt_type_keyword[i].type[j] != 255)
+				p[opt_type_keyword[i].type[j++]] = 1;
+			goto found;
+		}
+	}
+
+	/* Else try as a number */
+	while (*arg != '\0')
+	{
+		unsigned long val;
+		char *next;
+
+		val = strtoul(arg, &next, 0);
+		if (next == arg)
+		{
+			fprintf(stderr, "Invalid type keyword: %s\n", arg);
+			print_opt_type_list();
+			goto exit_free;
+		}
+		if (val > 0xff)
+		{
+			fprintf(stderr, "Invalid type number: %lu\n", val);
+			goto exit_free;
+		}
+
+		p[val] = 1;
+		arg = next;
+		while (*arg == ',' || *arg == ' ')
+			arg++;
+	}
+
+found:
+	return p;
+
+exit_free:
+	free(p);
+	return NULL;
+}
+
+
+
+/*
+ * Handling of option --string
+ */
+
+/* This lookup table could admittedly be reworked for improved performance.
+   Due to the low count of items in there at the moment, it did not seem
+   worth the additional code complexity though. */
+static const struct string_keyword opt_string_keyword[] = {
+	{ "bios-vendor", 0, 0x04 },
+	{ "bios-version", 0, 0x05 },
+	{ "bios-release-date", 0, 0x08 },
+	{ "system-manufacturer", 1, 0x04 },
+	{ "system-product-name", 1, 0x05 },
+	{ "system-version", 1, 0x06 },
+	{ "system-serial-number", 1, 0x07 },
+	{ "system-uuid", 1, 0x08 },             /* dmi_system_uuid() */
+	{ "baseboard-manufacturer", 2, 0x04 },
+	{ "baseboard-product-name", 2, 0x05 },
+	{ "baseboard-version", 2, 0x06 },
+	{ "baseboard-serial-number", 2, 0x07 },
+	{ "baseboard-asset-tag", 2, 0x08 },
+	{ "chassis-manufacturer", 3, 0x04 },
+	{ "chassis-type", 3, 0x05 },            /* dmi_chassis_type() */
+	{ "chassis-version", 3, 0x06 },
+	{ "chassis-serial-number", 3, 0x07 },
+	{ "chassis-asset-tag", 3, 0x08 },
+	{ "processor-family", 4, 0x06 },        /* dmi_processor_family() */
+	{ "processor-manufacturer", 4, 0x07 },
+	{ "processor-version", 4, 0x10 },
+	{ "processor-frequency", 4, 0x16 },     /* dmi_processor_frequency() */
+};
+
+static void print_opt_string_list(void)
+{
+	unsigned int i;
+
+	fprintf(stderr, "Valid string keywords are:\n");
+	for (i = 0; i < ARRAY_SIZE(opt_string_keyword); i++)
+	{
+		fprintf(stderr, "  %s\n", opt_string_keyword[i].keyword);
+	}
+}
+
+static int parse_opt_string(const char *arg)
+{
+	unsigned int i;
+
+	if (opt.string)
+	{
+		fprintf(stderr, "Only one string can be specified\n");
+		return -1;
+	}
+
+	for (i = 0; i < ARRAY_SIZE(opt_string_keyword); i++)
+	{
+		if (!strcasecmp(arg, opt_string_keyword[i].keyword))
+		{
+			opt.string = &opt_string_keyword[i];
+			return 0;
+		}
+	}
+
+	fprintf(stderr, "Invalid string keyword: %s\n", arg);
+	print_opt_string_list();
+	return -1;
+}
+
+
+/*
+ * Command line options handling
+ */
+
+/* Return -1 on error, 0 on success */
+int parse_command_line(int argc, char * const argv[])
+{
+	int option;
+	const char *optstring = "d:hqs:t:uV";
+	struct option longopts[] = {
+		{ "dev-mem", required_argument, NULL, 'd' },
+		{ "help", no_argument, NULL, 'h' },
+		{ "quiet", no_argument, NULL, 'q' },
+		{ "string", required_argument, NULL, 's' },
+		{ "type", required_argument, NULL, 't' },
+		{ "dump", no_argument, NULL, 'u' },
+		{ "dump-bin", required_argument, NULL, 'B' },
+		{ "from-dump", required_argument, NULL, 'F' },
+		{ "version", no_argument, NULL, 'V' },
+		{ 0, 0, 0, 0 }
+	};
+
+	while ((option = getopt_long(argc, argv, optstring, longopts, NULL)) != -1)
+		switch (option)
+		{
+			case 'B':
+				opt.flags |= FLAG_DUMP_BIN;
+				opt.dumpfile = optarg;
+				break;
+			case 'F':
+				opt.flags |= FLAG_FROM_DUMP;
+				opt.dumpfile = optarg;
+				break;
+			case 'd':
+				opt.devmem = optarg;
+				break;
+			case 'h':
+				opt.flags |= FLAG_HELP;
+				break;
+			case 'q':
+				opt.flags |= FLAG_QUIET;
+				break;
+			case 's':
+				if (parse_opt_string(optarg) < 0)
+					return -1;
+				opt.flags |= FLAG_QUIET;
+				break;
+			case 't':
+				opt.type = parse_opt_type(opt.type, optarg);
+				if (opt.type == NULL)
+					return -1;
+				break;
+			case 'u':
+				opt.flags |= FLAG_DUMP;
+				break;
+			case 'V':
+				opt.flags |= FLAG_VERSION;
+				break;
+			case '?':
+				switch (optopt)
+				{
+					case 's':
+						fprintf(stderr, "String keyword expected\n");
+						print_opt_string_list();
+						break;
+					case 't':
+						fprintf(stderr, "Type number or keyword expected\n");
+						print_opt_type_list();
+						break;
+				}
+				return -1;
+		}
+
+	/* Check for mutually exclusive output format options */
+	if ((opt.string != NULL) + (opt.type != NULL)
+	  + !!(opt.flags & FLAG_DUMP_BIN) > 1)
+	{
+		fprintf(stderr, "Options --string, --type and --dump-bin are mutually exclusive\n");
+		return -1;
+	}
+
+	if ((opt.flags & FLAG_FROM_DUMP) && (opt.flags & FLAG_DUMP_BIN))
+	{
+		fprintf(stderr, "Options --from-dump and --dump-bin are mutually exclusive\n");
+		return -1;
 	}
 
 	return 0;
 }
+
+
+void print_help(void)
+{
+	static const char *help =
+		"Usage: dmidecode [OPTIONS]\n"
+		"Options are:\n"
+		" -d, --dev-mem FILE     Read memory from device FILE (default: " DEFAULT_MEM_DEV ")\n"
+		" -h, --help             Display this help text and exit\n"
+		" -q, --quiet            Less verbose output\n"
+		" -s, --string KEYWORD   Only display the value of the given DMI string\n"
+		" -t, --type TYPE        Only display the entries of given type\n"
+		" -u, --dump             Do not decode the entries\n"
+		"     --dump-bin FILE    Dump the DMI data to a binary file\n"
+		"     --from-dump FILE   Read the DMI data from a binary file\n"
+		" -V, --version          Display the version and exit\n";
+
+	printf("%s", help);
+}
+
+
+
+int main(int argc, char * const argv[])
+{
+	int ret = 0;                /* Returned value */
+	int found = 0;
+	size_t fp;
+	int efi;
+	u8 *buf;
+
+	if (sizeof(u8) != 1 || sizeof(u16) != 2 || sizeof(u32) != 4 || '\0' != 0)
+	{
+		fprintf(stderr, "%s: compiler incompatibility\n", argv[0]);
+		exit(255);
+	}
+
+	/* Set default option values */
+	opt.devmem = DEFAULT_MEM_DEV;
+	opt.flags = 0;
+
+	if (parse_command_line(argc, argv)<0)
+	{
+		ret = 2;
+		goto exit_free;
+	}
+
+	if (opt.flags & FLAG_HELP)
+	{
+		print_help();
+		goto exit_free;
+	}
+
+	if (opt.flags & FLAG_VERSION)
+	{
+		printf("%s\n", VERSION);
+		goto exit_free;
+	}
+
+	if (!(opt.flags & FLAG_QUIET))
+		printf("# dmidecode %s\n", VERSION);
+
+	/* Read from dump if so instructed */
+	if (opt.flags & FLAG_FROM_DUMP)
+	{
+		if (!(opt.flags & FLAG_QUIET))
+			printf("Reading SMBIOS/DMI data from file %s.\n",
+			       opt.dumpfile);
+		if ((buf = mem_chunk(0, 0x20, opt.dumpfile)) == NULL)
+		{
+			ret = 1;
+			goto exit_free;
+		}
+
+		if (memcmp(buf, "_SM_", 4) == 0)
+		{
+			if (smbios_decode(buf, opt.dumpfile))
+				found++;
+		}
+		else if (memcmp(buf, "_DMI_", 5) == 0)
+		{
+			if (legacy_decode(buf, opt.dumpfile))
+				found++;
+		}
+		goto done;
+	}
+
+	/* First try EFI (ia64, Intel-based Mac) */
+	efi = address_from_efi(&fp);
+	switch (efi)
+	{
+		case EFI_NOT_FOUND:
+			goto memory_scan;
+		case EFI_NO_SMBIOS:
+			ret = 1;
+			goto exit_free;
+	}
+
+	if ((buf = mem_chunk(fp, 0x20, opt.devmem)) == NULL)
+	{
+		ret = 1;
+		goto exit_free;
+	}
+
+	if (smbios_decode(buf, opt.devmem))
+		found++;
+	goto done;
+
+memory_scan:
+	/* Fallback to memory scan (x86, x86_64) */
+	if ((buf = mem_chunk(0xF0000, 0x10000, opt.devmem)) == NULL)
+	{
+		ret = 1;
+		goto exit_free;
+	}
+
+	for (fp = 0; fp <= 0xFFF0; fp += 16)
+	{
+		if (memcmp(buf + fp, "_SM_", 4) == 0 && fp <= 0xFFE0)
+		{
+			if (smbios_decode(buf+fp, opt.devmem))
+			{
+				found++;
+				fp += 16;
+			}
+		}
+		else if (memcmp(buf + fp, "_DMI_", 5) == 0)
+		{
+			if (legacy_decode(buf + fp, opt.devmem))
+				found++;
+		}
+	}
+
+done:
+	if (!found && !(opt.flags & FLAG_QUIET))
+		printf("# No SMBIOS nor DMI entry point found, sorry.\n");
+
+	free(buf);
+exit_free:
+	free(opt.type);
+
+	return ret;
+}
+
